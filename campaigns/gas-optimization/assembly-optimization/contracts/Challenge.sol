a069fcff29f9e7138f460e00eb291991098a912e
Abhishek-2416 <abhishekalimchandani1624@protonmail.com>
2024-10-12 19:13:20 +0530

    Completed Gas Optimization course

--- campaigns/gas-optimization/assembly-optimization/contracts/Challenge.sol
+++ campaigns/gas-optimization/assembly-optimization/contracts/Challenge.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.19;
 
 abstract contract Challenge {
     /**
-     * @notice Returns a copy of the given array in a gas efficient way.
+     * @notice Returns a copy of the given array in a gas-efficient way.
      * @dev This contract will be called internally.
      * @param array The array to copy.
      * @return copy The copied array.
@@ -11,10 +11,25 @@ abstract contract Challenge {
     function copyArray(
         bytes memory array
     ) internal pure returns (bytes memory copy) {
-        copy = new bytes(array.length);
+        assembly {
+            // Create a new memory array with the same length as the original
+            let length := mload(array) // Load the length of the original array
+            copy := mload(0x40) // Get the free memory pointer
+            mstore(copy, length) // Store the length of the copied array
+            mstore(0x40, add(copy, add(0x20, length))) // Update the free memory pointer
 
-        for (uint256 i; i < array.length; i++) {
-            copy[i] = array[i];
+            // Copy the data from the original array to the new array
+            let dataOffset := add(array, 0x20) // Offset to the start of the data in the original array
+            let copyOffset := add(copy, 0x20) // Offset to the start of the data in the copied array
+
+            // Use a loop to copy each byte
+            for {
+                let i := 0
+            } lt(i, length) {
+                i := add(i, 0x20)
+            } {
+                mstore(add(copyOffset, i), mload(add(dataOffset, i)))
+            }
         }
     }
 }
