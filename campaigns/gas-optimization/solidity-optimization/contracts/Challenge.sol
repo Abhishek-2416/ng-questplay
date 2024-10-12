3d37d5d9e64a4e3d699af11c403c55e02627f531
Abhishek-2416 <abhishekalimchandani1624@protonmail.com>
2024-10-12 09:53:36 +0530

    Remove conflicting forge-std submodule

--- campaigns/gas-optimization/solidity-optimization/contracts/Challenge.sol
+++ campaigns/gas-optimization/solidity-optimization/contracts/Challenge.sol
@@ -2,24 +2,27 @@
 pragma solidity ^0.8.19;
 
 contract Challenge {
-
-    uint256 _SKIP;
+    uint256 immutable _SKIP;
 
     constructor(uint256 skip) {
         _SKIP = skip;
     }
 
-    /** 
+    /**
      * @notice Returns the sum of the elements of the given array, skipping any SKIP value.
      * @param array The array to sum.
      * @return sum The sum of all the elements of the array excluding SKIP.
      */
-    function sumAllExceptSkip(
-        uint256[] calldata array
-    ) public view returns (uint256 sum) {
-
+    function sumAllExceptSkip(uint256[] calldata array) public view returns (uint256 sum) {
         // IMPLEMENT HERE
-        
+        for (uint256 i = 0; i < array.length;) {
+            uint256 element = array[i];
+            if (element != _SKIP) {
+                sum += element;
+            }
+            unchecked {
+                ++i;
+            }
+        }
     }
-
 }
