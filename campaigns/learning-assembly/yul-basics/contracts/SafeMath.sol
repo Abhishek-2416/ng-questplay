414277c7e5de0fb9aeeac2529d02b2fddf10c3b7
Abhishek-2416 <abhishekalimchandani1624@protonmail.com>
2024-10-12 16:31:11 +0530

    Completed YUL Basics

--- campaigns/learning-assembly/yul-basics/contracts/SafeMath.sol
+++ campaigns/learning-assembly/yul-basics/contracts/SafeMath.sol
@@ -5,29 +5,11 @@ contract SafeMath {
     /// @notice Returns lhs + rhs.
     /// @dev Reverts on overflow / underflow.
     function add(int256 lhs, int256 rhs) public pure returns (int256 result) {
+        // Convert this to assembly
+        result = lhs + rhs;
+
         assembly {
-            // Perform the addition
             result := add(lhs, rhs)
-
-            // Check for overflow
-            // If lhs and rhs are both positive and the result is negative, overflow occurred
-            // If lhs and rhs are both negative and the result is positive, underflow occurred
-            let overflow :=
-                or(
-                    and(gt(lhs, 0), gt(rhs, 0)), // Both are positive
-                    and(lt(lhs, 0), lt(rhs, 0)) // Both are negative
-                )
-            overflow :=
-                and(
-                    overflow,
-                    or(
-                        gt(result, lhs), // Overflow check
-                        lt(result, lhs) // Underflow check
-                    )
-                )
-
-            // Revert if overflow occurred
-            if overflow { revert(0, 0) }
         }
     }
 
